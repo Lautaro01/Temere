64d549f71d5a6acbdbbaeb1c471dcbb08439bef8
Lautaro01 <lau123lau321@gmail.com>
2021-03-18 17:02:06 -0300

    Agrego funcion en numero random. Max Min, y toast de error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
@@ -20,7 +21,7 @@ import { AppRoutingModule } from './app-routing.module';
   NumeroRandomComponent,
   SorteoComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,FormsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/componentes/numero-random/numero-random.component.ts
+++ src/app/componentes/numero-random/numero-random.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-numero-random',
@@ -7,30 +8,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NumeroRandomComponent implements OnInit {
 
-  constructor() { }
+  constructor(public toastController: ToastController) { }
 
   numeroRandomVar = '?';
+  numeroMaximo = 100;
+  numeroMinimo = 1;
 
   numeroRandom() {
 
-    let numeroMaximo = 100;
-    let numeroMinimo = 1;
+    console.log(this.numeroMaximo);
+    if(this.numeroMaximo - this.numeroMinimo < 0)
+    {
+      this.presentToastWithOptions("Eso da negativo, no tiene sentido hacer esto.");
 
-    if(numeroMaximo - numeroMinimo < 0)
+    }else if (this.numeroMaximo - this.numeroMinimo == 0 )
     {
-      alert("Error! el numero Minimo no puede ser mayor al numero Maximo");
-      console.log("Error");
-    }else if (numeroMaximo - numeroMinimo == 0 )
-    {
-      alert("Vos entendes que es al pedo no? Tarado xd");
+      this.presentToastWithOptions("El maximo y el minimo no pueden ser iguales.");
     }
     else
     {
-      this.numeroRandomVar = Math.floor((Math.random() * (numeroMaximo - numeroMinimo + 1)) + numeroMinimo).toString();
+      this.numeroRandomVar = Math.floor((Math.random() * (this.numeroMaximo - this.numeroMinimo + 1)) + this.numeroMinimo).toString();
     }
     
 }
 
   ngOnInit() {}
+
+  async presentToastWithOptions(mensaje : string) {
+    const toast = await this.toastController.create({
+      header: 'Error',
+      message: mensaje,
+      color: 'danger',
+      position: 'top',
+      buttons: [
+        {
+          text: 'x',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    toast.present();
+  }
+
 
 }
